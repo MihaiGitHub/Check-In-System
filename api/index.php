0db9424e175de2965442f5efe1856d82e7f13f6e
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-09-06 17:43:31 -0700

    PHP API

---
+++ api/index.php
@@ -0,0 +1,14 @@
+<?php
+include 'dbconnect.php';
+
+if($_GET['clients'] == 'all'){
+    $stmt = $objDb->prepare('SELECT * FROM clients');
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $clients = $stmt->fetchAll();
+
+    echo json_encode(array(
+        'error' => false,
+        'clients' => $clients
+    ), JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP);
+}
