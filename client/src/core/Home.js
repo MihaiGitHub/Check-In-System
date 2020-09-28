a4da814e9174f5dae59f85289f3e7009373e6901
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-09-28 10:54:35 -0700

    Add login screen

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -1,43 +1,114 @@
 import React, { useState, useEffect } from "react";
-import { getSession } from "./apiCore";
+import { doLogin } from "./apiCore";
 import Main from "./Main";
 
 const Home = () => {
-  const [auth, setAuth] = useState(true);
+  const [auth, setAuth] = useState(false);
   const [error, setError] = useState(false);
+  // const [redirect, setRedirect] = useState(false);
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
 
-  const init = () => {
-    // getSession().then(({ data }) => {
-    //   if (!data.auth) {
-    //     setAuth(false);
-    //     setError(true);
-    //   }
-    // });
-  };
+  const { username, password } = values;
+
+  const init = () => {};
 
   useEffect(() => {
-    init();
+    // init();
   }, []);
 
-  const showLoginError = () => (
-    <div className="alert alert-danger mt-5" role="alert">
-      <h4 className="alert-heading">Login Error!</h4>
-      <p>
-        You must be logged in to the TNFP client system to access the Check In
-        System. Please login at the link below then refresh this page:
-      </p>
-      <hr />
-      <p className="mb-0">
-        <a href="https://www.tnfpapp.org/beta/#/login" target="_blank">
-          TNFP Login
-        </a>
-      </p>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    doLogin(values).then(({ data }) => {
+      if (data.jwt) {
+        sessionStorage.setItem("jwt", data.jwt);
+        setAuth(true);
+        //    setRedirect(true);
+      } else {
+        setError(true);
+      }
+    });
+  };
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const showError = () => (
+    <div className="alert alert-danger" role="alert">
+      <b>Error!</b> Invalid username or password.
+    </div>
+  );
+
+  const showLogin = () => (
+    <div className="cotainer" style={{ marginTop: 15 }}>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-header">Client Check In System</div>
+            <div className="card-body">
+              {error && showError()}
+              <form action="" method="">
+                <div className="form-group row">
+                  <label
+                    htmlFor="username"
+                    className="col-md-4 col-form-label text-md-right"
+                  >
+                    Username
+                  </label>
+                  <div className="col-md-6">
+                    <input
+                      onChange={handleChange("username")}
+                      type="text"
+                      id="username"
+                      className="form-control"
+                      name="username"
+                      required
+                      autoFocus
+                      value={username}
+                    />
+                  </div>
+                </div>
+
+                <div className="form-group row">
+                  <label
+                    htmlFor="password"
+                    className="col-md-4 col-form-label text-md-right"
+                  >
+                    Password
+                  </label>
+                  <div className="col-md-6">
+                    <input
+                      onChange={handleChange("password")}
+                      type="password"
+                      id="password"
+                      className="form-control"
+                      name="password"
+                      required
+                      value={password}
+                    />
+                  </div>
+                </div>
+
+                <div className="col-md-6 offset-md-4">
+                  <button onClick={handleSubmit} className="btn btn-primary">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 
   return (
     <main role="main" className="container" style={{ maxWidth: "100%" }}>
-      {error && showLoginError()}
+      {!auth && showLogin()}
       {auth && <Main />}
     </main>
   );
--- client/src/core/apiCore.js
+++ client/src/core/apiCore.js
@@ -1,6 +1,16 @@
 import { API } from "../config";
 import axios from "axios";
 
+export const doLogin = ({ username, password }) => {
+  return axios
+    .post(`${API}/user_login.php`, {
+      username,
+      password,
+    })
+    .then((response) => response)
+    .catch((error) => console.log(error));
+};
+
 export const getSession = () => {
   return axios
     .post(`${API}`)
@@ -14,8 +24,9 @@ export const getSession = () => {
 
 export const getClients = () => {
   return axios
-    .post(`XXXX`, {
-      jwt: "XXXX",
+    .post(`https://www.tnfpapp.org/beta/checkin/api/client_list.php`, {
+      jwt:
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiOCIsInVzZXJuYW1lIjoiZGVtbyIsInJvbGUiOiJhZG1pbiJ9fQ.7mmHx8yac4i5BbyyZuFZMaCOsNur0uAHM8gbk64euWU",
     })
     .then((response) => {
       return response;
