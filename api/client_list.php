89a5bf76aa74e7f9ffa9380994c548e8005ddd6e
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-09-30 15:45:48 -0700

    Update API end points

--- api/client_list.php
+++ api/client_list.php
@@ -7,7 +7,6 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // required to encode json web token
-include_once 'config/core.php';
 include_once 'libs/php-jwt-master/src/BeforeValidException.php';
 include_once 'libs/php-jwt-master/src/ExpiredException.php';
 include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
