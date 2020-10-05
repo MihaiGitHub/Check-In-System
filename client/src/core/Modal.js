8e070ed17c438966a5a8738b6c84eb0e411d8fac
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-10-04 20:33:18 -0700

    Serving component

--- client/src/core/Modal.js
+++ client/src/core/Modal.js
@@ -1,22 +1,13 @@
 import React from "react";
-import { updateClient, getClients } from "./apiCore";
-//import { Redirect } from "react-router-dom";
+import { updateClient } from "./apiCore";
 
-//const Modal = ({ client, type, refreshCheckin }) => {
 const Modal = ({ client, type, refreshFunction }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
     updateClient(client.id, type).then((response) => {
-      console.log("response ", response);
-
       refreshFunction();
-
-      //return <Redirect to="/" />;
     });
-
-    console.log("type", type);
-    console.log("client ", client);
   };
 
   return (
--- client/src/core/Serving.js
+++ client/src/core/Serving.js
@@ -1,67 +1,84 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import { getClients } from "./apiCore";
+import Modal from "./Modal";
 
 const Serving = () => {
-  const [client, setClient] = useState("");
+  const [clients, setClients] = useState([]);
+  const [client, setClient] = useState({});
+
+  useEffect(() => {
+    getClients("serving").then(({ data }) => {
+      if (!data.error) {
+        setClients(data.clients);
+      }
+    });
+  }, []);
+
+  const refreshCheckout = () => {
+    getClients("serving").then(({ data }) => {
+      if (!data.error) {
+        setClients(data.clients);
+      } else {
+        setClients([]);
+      }
+    });
+  };
 
   return (
     <Fragment>
-      <table className="table table-bordered table-striped mb-0">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            onClick={() => setClient("Mihai")}
-            data-toggle="modal"
-            data-id="1"
-            data-toggle="modal"
-            data-target="#exampleModalLong"
-          >
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">4</th>
-            <td>Mark</td>
-            <td>Otto</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">5</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">6</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">7</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-          <tr data-toggle="modal" data-id="1" data-target="#servingModal">
-            <th scope="row">8</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-          </tr>
-        </tbody>
-      </table>
+      {clients.length == 0 && (
+        <div className="row">
+          <div className="col-sm-6 offset-sm-3">
+            <div
+              className="alert alert-success"
+              role="alert"
+              style={{ textAlign: "center" }}
+            >
+              <strong>No clients being served</strong>
+            </div>
+          </div>
+        </div>
+      )}
+      {clients.length > 0 && (
+        <div className="row">
+          <div className="col-sm">
+            <table className="table table-bordered table-striped mb-0">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">First</th>
+                  <th scope="col">Last</th>
+                  <th scope="col">Number in family</th>
+                  <th scope="col">Specific request</th>
+                </tr>
+              </thead>
+              <tbody>
+                {clients.map((client, index) => (
+                  <tr
+                    data-id={client.id}
+                    id="modalLaunch"
+                    key={index}
+                    onClick={() => setClient(client)}
+                    data-toggle="modal"
+                    data-target="#clientModal"
+                  >
+                    <th scope="row">{index + 1}</th>
+                    <td>{client.fname}</td>
+                    <td>{client.lname}</td>
+                    <td>{client.familyNumber}</td>
+                    <td>{client.specificRequest}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+      <Modal
+        client={client}
+        type="checkout"
+        refreshFunction={refreshCheckout}
+      />
     </Fragment>
   );
 };
